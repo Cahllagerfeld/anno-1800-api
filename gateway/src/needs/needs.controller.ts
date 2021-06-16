f98a0912f5c6ed8beb56613720724c957b217d4c
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2021-06-16 20:08:20 +0200

    docs: update swagger

--- gateway/src/needs/needs.controller.ts
+++ gateway/src/needs/needs.controller.ts
@@ -1,19 +1,31 @@
 import { Body, Controller, Get, Inject, Param, Post } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
+import { ApiParam, ApiTags } from '@nestjs/swagger';
 
 @Controller('needs')
+@ApiTags('Needs')
 export class NeedsController {
   constructor(
     @Inject('NEEDS_SERVICE') private readonly needsService: ClientProxy,
   ) {}
 
   @Post(':lang/calculate')
+  @ApiParam({
+    name: 'lang',
+    type: 'string',
+    description: 'language in which the data should be retrieved',
+  })
   calculateNeeds(@Body() body, @Param('lang') lang: string) {
     console.log(lang);
     return this.needsService.send({ message: 'calculate' }, body);
   }
 
   @Post(':lang/optimize')
+  @ApiParam({
+    name: 'lang',
+    type: 'string',
+    description: 'language in which the data should be retrieved',
+  })
   optimizeFactorySet(@Body() body) {
     return this.needsService.send({ message: 'optimize' }, body);
   }
--- gateway/src/swagger.ts
+++ gateway/src/swagger.ts
@@ -2,6 +2,6 @@ import { DocumentBuilder } from '@nestjs/swagger';
 
 export const swaggerConfig = new DocumentBuilder()
   .setTitle('Anno 1800 Api')
-  .setDescription('The cats API description')
+  .setDescription('API Documentation for Anno 1800 API')
   .setVersion(process.env.npm_package_version)
   .build();

33a03d2a875b23bcc0bea0f80b49f9afaf5f4605
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2021-06-22 10:40:48 +0200

    feat: added config-service

--- mapping/src/main.ts
+++ mapping/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
     {
       transport: Transport.REDIS,
       options: {
-        url: 'redis://localhost:6379',
+        url: process.env.REDIS_URL,
       },
     },
   );

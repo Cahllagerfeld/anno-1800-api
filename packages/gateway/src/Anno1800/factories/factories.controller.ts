74ff42ed6f6786eec343af4e29468f5296359ffd
Cahllagerfeld <Cahllagerfeld>
2021-09-27 16:36:58 +0000

    feat: add internationalization

--- packages/gateway/src/Anno1800/factories/factories.controller.ts
+++ packages/gateway/src/Anno1800/factories/factories.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Query } from '@nestjs/common';
+import { ApiQuery, ApiTags } from '@nestjs/swagger';
+import ISO6391 from 'iso-639-1';
+import { languages } from '../utility/languages.enum';
+
+
 
 @Controller('factories')
 @ApiTags('Factories')
 export class FactoriesController {
+  constructor() { }
   @Get()
-  getAllFactories() {}
+  getAllFactories() { }
 
   @Get(':id')
-  getById() {}
+  @ApiQuery({ name: "lang", type: "string", required: false, enum: languages })
+  getById(@Query("lang") lang: string) {
+    console.log(ISO6391.getName(lang))
+  }
 }
